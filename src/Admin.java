8b4cf7eed4e290aec52340f5786d8b4d76a439c2
mohammedaminelahmami <lahmamimohammedamine@gmail.com>
2022-09-27 12:30:46 +0100

    fix

--- src/Admin.java
+++ src/Admin.java
@@ -1,19 +1,21 @@
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 public class Admin extends ConnectionDatabase{
     private String username;
     private String password;
     private String email;
-
+    protected ArrayList<String> arr = new ArrayList<String>();
+    protected String[][] arr2;
     public Admin()
     {
         //
     }
 
-    public boolean createStudent(String fullName, String username, String password)
+    public boolean createAccount(String table, String fullName, String username, String password)
     {
         try{
-            this.stmt = this.conn.prepareStatement("insert into apprenant (fullName, username, password) values (?, ?, ?)");
+            this.stmt = this.conn.prepareStatement("insert into "+table+" (fullName, username, password) values (?, ?, ?)");
             stmt.setString(1, fullName);
             stmt.setString(2, username);
             stmt.setString(3, password);
@@ -26,45 +28,56 @@ public class Admin extends ConnectionDatabase{
         }
     }
 
-    public void selectAllStudents(String label)
+    public String[][] selectAllAccounts(String table)
     {
         try{
-            this.stmt = this.conn.prepareStatement("select * from apprenant");
+            this.stmt = this.conn.prepareStatement("select * from " + table);
             ResultSet rs = stmt.executeQuery();
-
+            int i = 0;
             while(rs.next())
             {
-                System.out.println(rs.getString(label));
+                for(int j = 1; j < rs.getMetaData().getColumnCount(); j++)
+                {
+                    arr2[j][i] = new 
+                }
+                i++;
             }
             rs.close();
+            return arr2;
         }catch (Exception e)
         {
-            System.out.println(e);
+            System.out.println("error => " + e);
+            return arr2;
         }
     }
 
-    public void selectOneStudent(String label, int id)
+    public ArrayList<String> selectOneAccount(String table, int id)
     {
         try{
-            this.stmt = this.conn.prepareStatement("select * from apprenant where idApprenant = ?");
+            this.stmt = this.conn.prepareStatement("select * from " + table + " where id = ?");
             stmt.setInt(1, id);
             ResultSet rs = stmt.executeQuery();
 
+            // next() - 1 - 2 - 3 - 4 - 5
+
             while(rs.next())
             {
-                System.out.println(rs.getString(label));
+                for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+                    arr.add(rs.getString(i));
+                }
             }
-            rs.close();
+            return arr;
         }catch (Exception e)
         {
-            System.out.println(e);
+            System.out.println("error => " + e);
+            return arr;
         }
     }
 
-    public boolean updateStudent(String fullName, String username, String password)
+    public boolean updateAccount(String table, String fullName, String username, String password)
     {
         try {
-            this.stmt = this.conn.prepareStatement("update apprenant set fullName = ?, username = ?, password = ?");
+            this.stmt = this.conn.prepareStatement("update "+ table +" set fullName = ?, username = ?, password = ?");
             stmt.setString(1, fullName);
             stmt.setString(2, username);
             stmt.setString(3, password);
@@ -77,10 +90,10 @@ public class Admin extends ConnectionDatabase{
         }
     }
 
-    public boolean deleteStudent(int id)
+    public boolean deleteAccount(String table, int id)
     {
         try {
-            this.stmt = this.conn.prepareStatement("delete from apprenant where idApprenant = ?");
+            this.stmt = this.conn.prepareStatement("delete from "+ table +" where id = ?");
             stmt.setInt(1, id);
             int rs = stmt.executeUpdate();
             return rs == 1;
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         Admin admin = new Admin();
+        //admin.createAccount("formateur", "Youness Echchadi", "youness", "passYouness");
+        ArrayList<String> arr = admin.selectAllAccounts("formateur");
+        System.out.println(arr.toString());
+
+        //admin.updateAccount("formateur", "Youness Echchadi", "youness", "passYouness");
 
     }
 }
